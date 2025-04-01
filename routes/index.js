02fdbd0932e8e07258957cd5ded3a1067437bbfb
sofiasojoo <ssojo@udesa.edu.ar>
2025-04-01 17:42:21 -0300

    arregle imagenes + base db

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/product', function(req, res, next) {
+
+  res.render('product', { title: 'Express' });
+});
+
+router.get('/product/:id', function(req, res, next) {
+
+  res.render('product', { title: 'Express' });
+});
+
+router.get('/product/:id/comentarios', function(req, res, next) {
+
+  res.render('product', { title: 'Express' });
+});
+
+
+
 module.exports = router;
