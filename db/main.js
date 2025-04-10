69d751af667166b3c3112c87c0aa774d5937105a
paulabernsteinn <paulabernstein@Paulas-MBP.fibertel.com.ar>
2025-04-10 00:28:28 -0300

    swatch, productos db y css

--- db/main.js
+++ db/main.js
@@ -6,13 +6,13 @@ const objetoProductos = {
         password:"pablo123",
         fechaNacimiento: "06/07/2000",
         nDocumento: 40000000,
-        foto: ""
+        foto: "https://plus.unsplash.com/premium_photo-1689565611422-b2156cc65e47?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHBlcnNvbmF8ZW58MHx8MHx8fDA%3D"
     },
     productos: [
         {
-            imagen: "",
-            nombre: "",
-            descripcion: "",
+            imagen: "https://static.swatch.com/images/product/YLS236G/sa200/YLS236G_sa200_er005.png",
+            nombre: "Petal Swirl",
+            descripcion: "Este elegante reloj de acero inoxidable tiene una esfera amarilla cepillada al sol con un motivo floral impreso en amarillo claro, aguja horaria y minutero plateados que brillan en la oscuridad y segundero plateado.",
             comentarios: [{
                 nombres: "",
                 texto: "",
@@ -20,9 +20,9 @@ const objetoProductos = {
             },]
         },
         {
-            imagen: "",
-            nombre: "",
-            descripcion: "",
+            imagen: "https://static.swatch.com/images/product/YLS235G/sa200/YLS235G_sa200_er005.png",
+            nombre: "Petal Frenzy",
+            descripcion: "Este elegante reloj de acero inoxidable tiene una esfera azul cepillada al sol con un motivo floral impreso en azul claro, aguja horaria y minutero plateados que brillan en la oscuridad y segundero plateado. ",
             comentarios: [{
                 nombres: "",
                 texto: "",
@@ -30,9 +30,9 @@ const objetoProductos = {
             },]
         },
         {
-            imagen: "",
-            nombre: "",
-            descripcion: "",
+            imagen: "https://static.swatch.com/images/product/YLS237G/sa200/YLS237G_sa200_er005.png",
+            nombre: "Petal Bliss",
+            descripcion: "Este elegante reloj de acero inoxidable tiene una esfera gris cepillada al sol con un motivo floral impreso en blanco, aguja horaria y minutero plateados que brillan en la oscuridad y segundero plateado. ",
             comentarios: [{
                 nombres: "",
                 texto: "",
@@ -40,9 +40,9 @@ const objetoProductos = {
             },]
         },
         {
-            imagen: "",
-            nombre: "",
-            descripcion: "",
+            imagen: "https://static.swatch.com/images/product/YLS234G/sa200/YLS234G_sa200_er005.png",
+            nombre: "Petal Charm",
+            descripcion: "Este elegante reloj de acero inoxidable tiene una esfera rosa cepillada al sol con un motivo floral impreso en blanco, aguja horaria y minutero plateados que brillan en la oscuridad y segundero plateado.",
             comentarios: [{
                 nombres: "",
                 texto: "",
@@ -50,9 +50,9 @@ const objetoProductos = {
             },]
         },
         {
-            imagen: "",
-            nombre: "",
-            descripcion: "",
+            imagen: "https://static.swatch.com/images/product/YLS235/sa200/YLS235_sa200_er005.png",
+            nombre: "Blue Petal Promise",
+            descripcion: "Este elegante reloj de acero inoxidable tiene una esfera azul cepillada al sol con un motivo floral impreso en azul claro, aguja horaria y minutero plateados que brillan en la oscuridad y segundero plateado.",
             comentarios: [{
                 nombres: "",
                 texto: "",
@@ -60,9 +60,9 @@ const objetoProductos = {
             },]
         },
         {
-            imagen: "",
-            nombre: "",
-            descripcion: "",
+            imagen: "https://static.swatch.com/images/product/YLS234/sa200/YLS234_sa200_er005.png",
+            nombre: "Pink Petal Promise",
+            descripcion: "Este elegante reloj de acero inoxidable tiene una esfera rosa cepillada al sol con un motivo floral impreso en blanco, aguja horaria y minutero plateados que brillan en la oscuridad y segundero plateado.",
             comentarios: [{
                 nombres: "",
                 texto: "",
@@ -70,9 +70,9 @@ const objetoProductos = {
             },]
         },
         {
-            imagen: "",
-            nombre: "",
-            descripcion: "",
+            imagen: "https://static.swatch.com/images/product/SO34R700/sa200/SO34R700_sa200_er005.png",
+            nombre: "Squarely Berry",
+            descripcion: "La caja cuadrada de color rosa oscuro mate de este reloj de Bioceramic está cubierta por un cristal de material de origen biológico de borde a borde que ofrece una vista lateral de la pista de minutos impresa en rosa claro.",
             comentarios: [{
                 nombres: "",
                 texto: "",
@@ -80,9 +80,9 @@ const objetoProductos = {
             },]
         },
         {
-            imagen: "",
-            nombre: "",
-            descripcion: "",
+            imagen: "https://static.swatch.com/images/product/SO34N700/sa200/SO34N700_sa200_er005.png",
+            nombre: "Squarely Blacklight",
+            descripcion: "La caja cuadrada de color azul oscuro mate de este reloj de Bioceramic está cubierta por un cristal de material de origen biológico de borde a borde que ofrece una vista lateral de la pista de minutos impresa en azul claro.",
             comentarios: [{
                 nombres: "",
                 texto: "",
@@ -90,9 +90,9 @@ const objetoProductos = {
             },]
         },
         {
-            imagen: "",
-            nombre: "",
-            descripcion: "",
+            imagen: "https://static.swatch.com/images/product/YLS468G/sa200/YLS468G_sa200_er005.png",
+            nombre: "Middlesteel",
+            descripcion: "Ni grande, ni pequeño: MIDDLESTEEL (YLS468G) tiene el tamaño perfecto y una esfera verde oscuro con apliques de Swatch y ventana de fecha. El brazalete de acero inoxidable va a juego con la caja.",
             comentarios: [{
                 nombres: "",
                 texto: "",
@@ -100,21 +100,16 @@ const objetoProductos = {
             },]
         },
         {
-            imagen: "",
-            nombre: "",
-            descripcion: "",
+            imagen: "https://static.swatch.com/images/product/YLS455G/sa200/YLS455G_sa200_er005.png",
+            nombre: "Pinkaround",
+            descripcion: "Es tiempo de amar con PINKAROUND (YLS455G). La espectacular esfera rosa satinada con impresiones en blanco y gris incluye una ventana que muestra la fecha a las 3 para que no se te olvide nunca tu cita con la persona deseada.",
             comentarios: [{
                 nombres: "",
                 texto: "",
                 imagen: ""
             },]
         },
-
     ]
 }
 
 module.exports= objetoProductos
-
-//aca hay que hacer que los datos vengan de lo que se rellena en 
-//el formulario, arme la base para poder hacer otras 
-//cosas pero no hice los formularios pq pau me dijo que los podia hacer ella
